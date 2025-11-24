764b3a0442ff3a8b3e49bc0161c05803cad35492
DivyaDarji1804 <divya.darji2004@gmail.com>
2025-11-24 11:00:52 +0000

    commit again

--- 01_basics/stingPractice.js
+++ 01_basics/stingPractice.js
@@ -1,3 +1,8 @@
+// in following file mentioned of the string and its different functions and methods most are method except the .length 
+// reference is w3 school and chai aur code 
+
+
+
 // let str1 = `Hello world`;
 
 // console.log(str1);
@@ -26,7 +31,7 @@
 
 // slice is use to extracr a part of string you want
 
-let text = "Apple, Banana, Kiwi";
+ let text = "Apple, Banana, Kiwi";
 // console.log(text.slice(7,13));
 
 // // if we just right single digit then it will print after that num like after 5 length rest will printed but not first 5
@@ -41,7 +46,65 @@ let text = "Apple, Banana, Kiwi";
 // console.log(text.slice(-12,-6));
 
 
-// // substring do same
+// // substring do same just minus value considers as zeros
 
 // console.log(text.substring(-5));
 // console.log(text.substring(0,13));
+
+
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+// console.log(text.isWellFormed());
+
+// let txt2 = `this is  test  \uD800`;
+// console.log(txt2.isWellFormed());
+
+// //towellFormed will make this proper
+// console.log(txt2.toWellFormed());
+
+
+// trim is use to remove spaces from both side of string 
+
+// let bigString = `         Helllo world                     `;
+// console.log(bigString.trim());
+// console.log(bigString.trimStart());
+// console.log(bigString.trimEnd());
+
+
+// padStart() and padEnd use to add  pads anothe string until it reaches the given length 
+
+// let num1= 5;
+// let str =num1.toString();
+
+// console.log(str.padStart(5,"D"));
+
+// console.log(str.padEnd(11,"o"));
+
+// repeat is use to repeact a sting 
+
+// let greetings= "Good Morning!!! ";
+// console.log(greetings.repeat(4));
+
+// replace is use to replace a string  specific
+
+// let str = `hello i am johny . Johny is my first name willams is second`;
+// console.log(str.replace("johny","roman"));
+// // for the case cencitivity using / at start and /gi at end instead of ""
+// console.log(str.replace(/johny/gi,"roman"));
+
+// let text23 = "I love cats. Cats are very easy to love. Cats are very popular.";
+// console.log(text23.replaceAll(/cats/gi,"wolfs"));
+
+
+// to convert a string to array we can use split()
+
+//  text = "Apple, Banana, Kiwi";
+//  let arrText = text.split(",");
+//  console.log(typeof arrText);
+//  console.log(arrText);
+//  console.log(arrText[1]);
+
+// split(",")
+// split(" ")
+// split("|")
