9ff461e4b4766383b0e6c95f645eb479f353e5fd
DivyaDarji1804 <divya.darji2004@gmail.com>
2025-11-24 12:30:23 +0000

    numbers done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,38 @@
+// let score = 264;
+// console.log(score);
+// const balance = new Number(100);
+// console.log(balance.toString().length);
+// let pi = 3.14257;
+
+// // toFixed fixes floating points 
+// console.log(pi.toFixed(2));
+
+// let anotherNum = 1423.8966
+// //precision is use to consider before floating points also
+// console.log(anotherNum.toPrecision(3));
+
+// toLocalString use to make value in properformat
+
+// const amount = 100000000;
+
+// console.log(amount.toLocaleString()); //us standards
+// console.log(amount.toLocaleString('en-IN')); //Inidan standards
+
+
+
+// ++++++++++ Maths +++++++++++++
+
+// console.log(Math.abs(-122.45));
+// console.log(Math.round(3.148)); // must mostly used 
+// console.log(Math.ceil(3.148));
+// console.log(Math.floor(3.148));
+// console.log(Math.sqrt(144));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min =10
+const max =20 
+
+
+console.log(Math.floor(Math.random() * (max -min +1))+ min);
