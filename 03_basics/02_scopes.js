6fa8fc7fa58d33b579589ec0c9ee2590271d1326
DivyaDarji1804 <divya.darji2004@gmail.com>
2025-11-27 11:37:10 +0000

    scope basics

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,3 +1,11 @@
+/*
+learn basic of what is global and local scope 
+also basic of hositing just level of understanding not in code 
+*/
+
+
+
+
 let a=10; // global declare
 const b=20;
 var c=30; 
@@ -5,9 +13,9 @@ var c=30;
 if (true){   
     let a = 25;  // block  for this loop 
 }
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 
 
@@ -22,3 +30,46 @@ let a = 15;  // this is wrong
 }
 let a=23;
 */
+
+
+
+// ----------- scope level lecture -22 ------------------------------------
+
+
+function one(){
+    const userName = "hitesh";
+
+    function two(){
+        const website = "youtube";
+        console.log(userName);
+        console.log(website);
+    }
+    two();
+}
+// one();
+
+
+
+if(true){
+    const username ="hitesh";
+    if (username === "hitesh"){
+        const web = " youtube";
+        // console.log(username + web)
+    }
+    // console.log(web);
+}
+// console.log(username);
+
+
+// ++++++++++++++++++ interseting  +++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5));
+function addOne(num){
+ return num+1;
+}
+
+
+const addTwo = function (num){
+    return num+2;
+}
+console.log(addTwo(5));
