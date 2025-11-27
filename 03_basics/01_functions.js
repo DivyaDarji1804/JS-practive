70529e0946d875d971d2eed0427dab8b0bf55224
DivyaDarji1804 <divya.darji2004@gmail.com>
2025-11-27 09:19:49 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,9 +1,11 @@
 /*
---------- quick notes ----------
+--------- quick notes f both lectures ----------
 learn how to crete a funtion 
 how to pass argument 
 how to add if else condition
 how to pass default value of parameter and overwrite it 
+"..." is known as rest or spread operator 
+also how to pass object and array into the function
 */
 function sayMyName (){
     console.log("walter white");
@@ -45,4 +47,48 @@ function loginUserMessage (userName = "sam"){
 
 
 const msg = loginUserMessage("Heisenberg");
-console.log(msg);
+// console.log(msg);
+
+
+
+// ---------------------------- lecture 2 of function -----------------------------------------
+
+function calculateCartPrice (...num1) {  //rest operator 
+ return num1;
+}
+
+// console.log(calculateCartPrice(200,440,500)); 
+
+
+// function calculateCartPrice (val1,val2,...num1) {  //rest operator 
+//  return num1;
+// }
+
+// console.log(calculateCartPrice(200,440,500,2000)); 
+
+
+const user = {
+    userName : "Hitesh",
+    price : 399
+}
+
+function handleObject(anyObject){
+ console.log(`user name is ${anyObject.userName} and price is ${anyObject.price}`)
+}
+
+// handleObject(user);
+
+// handleObject({
+//     userName : "Rajesh",
+//     price : 1299
+// });
+
+
+const myArr = [200,400,100,600];
+
+function returnSecondValueOfArr (getArray){
+   return getArray[1];
+}
+
+// console.log(returnSecondValueOfArr(myArr));
+console.log(returnSecondValueOfArr([100,456,44,3443]));
