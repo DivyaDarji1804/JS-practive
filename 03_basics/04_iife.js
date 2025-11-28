5ea306f4e8ad27179ed41cccce70b0a6874e39b9
DivyaDarji1804 <divya.darji2004@gmail.com>
2025-11-28 05:51:38 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+/**
+ ---------- quick notes --------------
+ // Immediately Invoked Function Expression (IIFE)
+ it use to avoid any global conflit also to call any function at a time
+ iife means to avoid global scope poluttion and to call any function immediiattly we use  immediatly invoked function expression
+
+syntax : 
+( your_function ) ()
+
+ */
+
+(function check (){
+    console.log(`DB connected`);
+})()
