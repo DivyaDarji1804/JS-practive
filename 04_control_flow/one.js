0a7fd21501dafbd998c311e8f575d26a75d9c0a5
DivyaDarji1804 <divya.darji2004@gmail.com>
2025-12-01 10:56:11 +0000

    watch till 14 minutes

---
+++ 04_control_flow/one.js
@@ -0,0 +1,24 @@
+/*
+-------------- control flow quick notes --------------------------
+learn about if , if-else , 
+watch till 14 mintues
+*/
+
+// const isUserLoggedIn = true;
+// const temperature = 32; 
+
+
+// if(temperature < 50){
+//     console.log(`temperature is less than 50`);
+// }
+// else{
+//     console.log(`temperature is greater than 50`);
+// }
+
+
+const score = 200;
+
+if (score > 100){
+    const power = 'fly';
+    console.log(`user power : ${power}`);
+}
